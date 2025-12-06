362d6f6b40e9f840415927b17f839cb1e81eee3c
salcines <salcines@outlook.es>
2025-12-06 23:10:55 +0100

    Random Class. Task 4 - Generate random numbers from lower to upper both include

--- Topics/Random class/Generate random numbers/src/Main.java
+++ Topics/Random class/Generate random numbers/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int numbers = scanner.nextInt();
+        int lowerBound = scanner.nextInt();
+        int upperBound = scanner.nextInt();
+
+        int total = 0;
+
+        Random random = new Random(lowerBound + upperBound);
+
+        for (int i = 0; i < numbers; i++) {
+            int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            total += randomNumber;
+        }
+
+        System.out.println(total);
+        scanner.close();
     }
 }
