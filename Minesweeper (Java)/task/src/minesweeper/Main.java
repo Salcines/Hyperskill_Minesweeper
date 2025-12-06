670fd1426fb945070586893f2fd7edfefee77e60
salcines <salcines@outlook.es>
2025-12-06 22:08:05 +0100

    Stage 1 - Lay the groundwork. Place a symbols with mines and safe

--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -1,8 +1,69 @@
 package minesweeper;
 
-public class Main {
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-       //write your code here
+enum CellState {
+    MINE ('X'),
+    SAFE ('.');
+
+    private final char symbol;
+
+    CellState(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
     }
 }
+
+public class Main {
+    public static final int MINES_COUNT = 10;
+
+    public static void main(String[] args) {
+        CellState[][] field = initializeField();
+        placeMines(field);
+        printField(field);
+
+    }
+
+    private static void placeMines(CellState[][] field) {
+        int mines = 0;
+        int index = 0;
+        int skip = 5;
+
+        int totalCells = field.length * field[0].length;
+
+        while (mines < MINES_COUNT && index < totalCells) {
+            int row = index / field[0].length;
+            int column = index % field[0].length;
+
+            if (index % skip == 0 && field[row][column] == CellState.SAFE) {
+                field[row][column] = CellState.MINE;
+                mines++;
+            }
+
+            index++;
+        }
+    }
+
+    private static CellState[][] initializeField() {
+        CellState[][] field = new CellState[9][9];
+
+        for (CellState[] row : field) {
+            Arrays.fill(row, CellState.SAFE);
+        }
+        return field;
+    }
+
+    private static void printField(CellState[][] field) {
+        for (CellState[] row : field) {
+            for (CellState cell : row) {
+                System.out.print(cell.getSymbol());
+            }
+
+            System.out.println();
+        }
+    }
+}
+
