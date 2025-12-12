beca61146c79f64b8146571c3dad29cb40ff42db
salcines <salcines@outlook.es>
2025-12-12 22:05:32 +0100

    Introduction to generic programming - Task 1. Change the unlimited box classes definition for a generic one

---
+++ Topics/Introduction to generic programming/Pastry packing optimization/src/Main.java
@@ -0,0 +1,18 @@
+class Box<T> {
+    private T box;
+    public void put (T box) {
+        this.box = box;
+    }
+    public T get() {
+        return this.box;
+    }
+}
+
+
+
+// Don't change classes below
+class Cake { }
+
+class Pie { }
+
+class Tart { }
